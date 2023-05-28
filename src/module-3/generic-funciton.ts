0ceda412856ed01350eea56c7f62c6c3b16f8831
mamun-prodhan <mamunpr9@gmail.com>
2023-05-29 00:02:18 +0600

    generic function in ts

---
+++ src/module-3/generic-funciton.ts
@@ -0,0 +1,18 @@
+// arrow function
+const createArray = (param: string): string[] => {
+    return [param];
+};
+
+const createArray1 = <T>(param: T): T[] =>{
+    return [param];
+}
+
+const createArrayTuple = <X, Y>(param1: X, param2: Y) : [X, Y] =>{
+    return [param1, param2];
+}
+
+const result1 = createArrayTuple<string, string>("Bangladesh", "Pakistan");
+
+const result2 = createArrayTuple<boolean, Array<string>>(false, ["USA"]);
+
+const result3 = createArray1<{name: string}>({name: 'Bangladesh'});
--- src/module-3/generic-interface.ts
+++ src/module-3/generic-interface.ts
@@ -1,47 +1,47 @@
-// Generic Interface
-interface CrushInterface<T, U = null>{
-    name: string;
-    husband: T;
-    wife?:U;
-}
+// // Generic Interface
+// interface CrushInterface<T, U = null>{
+//     name: string;
+//     husband: T;
+//     wife?:U;
+// }
 
-interface PersonInterface{
-    name: string;
-    age: number;
-}
+// interface PersonInterface{
+//     name: string;
+//     age: number;
+// }
 
-const crush4: CrushInterface<PersonInterface, PersonInterface> = {
-     name: 'Kate',
-     husband: {
-        name: 'mamun',
-        age: 24
-     },
-     wife: {
-        name: 'winslet',
-        age: 40
-     }
-}
+// const crush4: CrushInterface<PersonInterface, PersonInterface> = {
+//      name: 'Kate',
+//      husband: {
+//         name: 'mamun',
+//         age: 24
+//      },
+//      wife: {
+//         name: 'winslet',
+//         age: 40
+//      }
+// }
 
-const crush1: CrushInterface<boolean, string> = {
-    name: 'Kate Winslet',
-    husband: true,
-    wife: "sokina"
-}
+// const crush1: CrushInterface<boolean, string> = {
+//     name: 'Kate Winslet',
+//     husband: true,
+//     wife: "sokina"
+// }
 
-const crush2: CrushInterface<string> = {
-    name: 'purnima',
-    husband: 'riaz ahamed'
-}
+// const crush2: CrushInterface<string> = {
+//     name: 'purnima',
+//     husband: 'riaz ahamed'
+// }
 
-interface IHusband{
-    name: string;
-    salary: number;
-}
+// interface IHusband{
+//     name: string;
+//     salary: number;
+// }
 
-const crush3: CrushInterface<IHusband> = {
-    name: 'sabnoor',
-    husband: {
-        name: 'Persian',
-        salary: 50000
-    }
-}
+// const crush3: CrushInterface<IHusband> = {
+//     name: 'sabnoor',
+//     husband: {
+//         name: 'Persian',
+//         salary: 50000
+//     }
+// }
