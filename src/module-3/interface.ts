f850485a5b04d9d935d23a197c8e7ef17e92c438
mamun-prodhan <mamunpr9@gmail.com>
2023-05-28 21:04:35 +0600

    interface

--- src/module-3/interface.ts
+++ src/module-3/interface.ts
@@ -2,11 +2,29 @@ type User = {
     name: string;
     age: number;
 };
+type extendedUser2 = User & {
+    role: string;
+};
 
 interface IUser {
     name: string;
     age: number;
 };
+
+interface IExtendedUser extends IUser{
+    role: string;
+}
+
+const extendedUser: IExtendedUser = {
+    name: "Mamun",
+    age: 32,
+    role: "admin"
+}
+const extendedUsers: extendedUser2 = {
+    name: "dipu",
+    age: 33,
+    role: "student"
+}
 
 const userWithTypeAlias: User = {
     name: "Type Alias",
@@ -17,3 +35,9 @@ const userWithInterface: IUser = {
     name: "Interface",
     age: 200,
 };
+
+
+type addNumbersType = (num1: number, num2: number) => number;
+const addNumbers:addNumbersType = (num1, num2) => num1 + num2;
+
+console.log(addNumbers(10,20));
