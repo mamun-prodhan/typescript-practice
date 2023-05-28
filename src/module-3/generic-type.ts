2852abab6a71ab4ea9b72b6ec43f034026648125
mamun-prodhan <mamunpr9@gmail.com>
2023-05-28 22:11:39 +0600

    generic typed

--- src/module-3/generic-type.ts
+++ src/module-3/generic-type.ts
@@ -1,6 +1,18 @@
 type GenericTuple<X, Y> = [X, Y];
 const relation: GenericTuple<string, string> = ['Mamun', 'Rashmika Mandana'];
 
+type relationWithSalaryType = {name: string; salary: number};
+
+const relationWithSalary: GenericTuple<relationWithSalaryType, string> = [
+    { 
+        name: 'Mamun',
+        salary: 12000
+    },
+    "Kate Winslet",
+];
+
+
+ 
 type GenericArray<T> = Array<T>;
 
 const rollNumbers10: GenericArray<number> = [44, 55, 20];
