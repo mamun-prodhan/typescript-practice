9b1a0d157d4b46c8201fbf7ec720e868a2537a73
mamun-prodhan <mamunpr9@gmail.com>
2023-05-28 21:40:37 +0600

    generic type in typescript

---
+++ src/module-3/generic-type.ts
@@ -0,0 +1,14 @@
+const rollNumbers10: Array<number> = [44, 55, 20];
+const allNames: Array<string> = ['dipu', 'drupal', 'disha'];
+const isStudent: Array<boolean> = [true, false, true, false];
+
+const userNameAndRollNumbers: Array<{name: string; roll: number}> = [
+    {
+        name: 'mamun pro',
+        roll: 1,
+    },
+    {
+        name: 'shovon sk',
+        roll: 2,
+    }
+]
