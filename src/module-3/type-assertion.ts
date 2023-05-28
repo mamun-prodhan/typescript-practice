73201aa2fd98a31a6aa14d94c42358152c08d81d
mamun-prodhan <mamunpr9@gmail.com>
2023-05-28 12:14:56 +0600

    type assertion in ts

--- src/module-3/type-assertion.ts
+++ src/module-3/type-assertion.ts
@@ -1,5 +1,34 @@
-  let emni: any;
+//type assertion
+let emni: any;
+emni = "Next Level Web Dev";
+console.log((emni as string).length);
+<string>emni.length;
 
-  emni = "Next Level Web Dev";
 
-  console.log((emni as string).length);
+function kgToGram(param: string | number): string | number | undefined{
+    if(typeof param === "string"){
+        const value = parseFloat(param) * 1000;
+        return `The Converted result is : ${value} gram`;
+    }
+    if(typeof param === "number"){
+        const value = param * 1000;
+        return value;
+    }
+}
+
+const resultToBeNumber = kgToGram(100) as number;
+// const resultToBeNumber = <number> kgToGram(100);
+const resultToBeString = <string> kgToGram(50);
+
+
+type CustomErrorType = {
+    message: string;
+}
+
+
+try{
+
+}
+catch(err){
+    console.log((err as CustomErrorType).message)
+}
