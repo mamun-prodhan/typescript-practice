d554f70bac61d71f38eb711520d9c76236c61c53
mamun-prodhan <mamunpr9@gmail.com>
2023-05-28 10:39:58 +0600

    Nullish Coalescing operator in typescript

--- src/ternaryOperator.ts
+++ src/ternaryOperator.ts
@@ -18,3 +18,27 @@ const isAuthenticatedUser = "";
 const userName = isAuthenticatedUser ?? "Guest";
 const userName2 = isAuthenticatedUser ? isAuthenticatedUser : "Guest 2";
 console.log({userName} , {userName2});
+
+
+
+type Manush = {
+    name: string;
+    age: number;
+    address: {
+        city: "No city";
+        road: "No Road";
+        home?: "";
+    };
+};
+
+const manush1: Manush = {
+    name: "Mamun",
+    age: 22,
+    address: {
+        city: "No city",
+        road: "No Road",
+    },
+};
+
+const home = manush1?.address?.home ?? "No Home"; //defalult "NO HOme"
+console.log({home});
