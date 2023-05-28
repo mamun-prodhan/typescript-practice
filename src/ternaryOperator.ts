36e5f0d12c614d341d4f02be0d02185bc4acee75
mamun-prodhan <mamunpr9@gmail.com>
2023-05-28 09:22:58 +0600

    ternary operator in typescript

---
+++ src/ternaryOperator.ts
@@ -0,0 +1,12 @@
+//ternary operator
+const age: number = 12;
+
+if(age >=18){
+    console.log("Yes");
+}
+else{
+    console.log("NO");
+}
+
+const isAdult = age >= 18 ? "Yes, He is Adult" : "No, Not adult";
+console.log(isAdult);
