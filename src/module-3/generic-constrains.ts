65dcb5fe6eacd199f806f54cd6e2769cb2c3747e
mamun-prodhan <mamunpr9@gmail.com>
2023-05-29 14:17:01 +0600

    generic constrains

--- src/module-3/generic-constrains.ts
+++ src/module-3/generic-constrains.ts
@@ -1,14 +1,24 @@
-const addMetoCrush = <T>(myInfo: T) =>{
+const addMetoCrush = <T extends {name: string, age: number, salary: number}>(myInfo: T) =>{
     const crush = "kate winslet";
     const newData = {...myInfo, crush};
     return newData;
 };
 
-const myInfo = {
+type MyInfoType = {
+    name: string;
+    age: number;
+    salary: number;
+    gender: string;
+    other: number;
+}
+
+const myInfo:MyInfoType = {
     name: "Mamun",
     age: 100,
     salary: 100000,
+    gender: "male",
+    other: 565464
 };
 
-const result6 = addMetoCrush(myInfo);
+const result6 = addMetoCrush<MyInfoType>(myInfo);
 console.log(result6);
